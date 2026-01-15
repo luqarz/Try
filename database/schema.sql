17849c9ad473395dda71fbb4ac5841cc2e926f4e
luqarz <lucasmbuthia@gmail.com>
2026-01-15 05:17:54 +0300

    Create schema.sql

---
+++ database/schema.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE my_app_db;
+USE my_app_db;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100),
+    email VARCHAR(100) UNIQUE,
+    password VARCHAR(255),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
